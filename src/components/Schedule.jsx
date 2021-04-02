20c00e38cafa142853b2327905c23a58a4c16867
emae1712 <melissa.aechegaray@gmail.com>
2021-04-02 11:25:46 -0500

    toggle card

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Image from "../img/Figures.jpg";
 
 const Schedule = ({category, cards}) =>{
 	console.log(cards.Ideation);
-	const allcheckValidation = () =>{
-		cards.Validation.map((element) => {
-        element.checked = true;
-				console.log(element);
-      return element;
+
+	const [isCheck, setIsCheck] = useState(false);
+
+	// toggle card
+	const toggle = (inputName) => {
+    setIsCheck((prevState) => {
+      const newState = { ...prevState };
+      newState[inputName] = !prevState[inputName];
+      return newState;
     });
-	}
+  };
+	console.log(isCheck);
 
     return (
       <>
 			
 				<h2>{category === 'socialInnovation'? 'Social Innovation' : ''}</h2>
 				{category === 'socialInnovation'&& cards.Ideation.concat(cards.Validation).map((card) =>(
-          <label>
-            <input type="radio" name={card.title} value={card.title}/>
-            <div>
+          <label key = {card.id}>
+            <input id = {card.id} type="radio" name={card.title} value={card.title} onClick={() => toggle(card.title)} checked={isCheck[card.title]}/>
+            <div >
 							{card.title}
 							
 							<p>view builder</p>
@@ -34,8 +39,8 @@ const Schedule = ({category, cards}) =>{
 			
 				<h2>{category === 'all'? 'Ideation' : ''}</h2>
 				{category === 'all'&& cards.Ideation.map((card) =>(
-          <label>
-            <input type="radio" name={card.title} value={card.title}/>
+          <label key = {card.id}>
+            <input id = {card.id} type="radio" name={card.title} value={card.title} onClick={() => toggle(card.title)} checked={isCheck[card.title]}/>
             <div>
 							{card.title}
 							
@@ -51,12 +56,12 @@ const Schedule = ({category, cards}) =>{
 					))}
 
 				<h2>{category === 'all'? 'Validation' : ''}</h2>
-				<button onChange={() => allcheckValidation()}>select all in ideation</button>
+				<button onChange=''>select all in ideation</button>
 				{category === 'all'&& cards.Validation.map((card) =>(
 					<>
 					
-          <label>
-            <input type="radio" name={card.title} value={card.title}/>
+          <label key = {card.id}>
+            <input id = {card.id} type="radio" name={card.title} value={card.title} onClick={() => toggle(card.title)} checked={isCheck[card.title]}/>
             <div>
 							{card.title}
 							
