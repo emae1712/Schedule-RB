56f2b5a66c8d4f7671e823a285e8a11ead659b50
emae1712 <melissa.aechegaray@gmail.com>
2021-04-03 06:06:34 -0500

    improve styles and some things

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -8,7 +8,9 @@ const Schedule = ({category, cards}) =>{
 	console.log(cards.Ideation);
   //data
   const socialData = Data.Ideation.concat(Data.Validation);
-	const [socialInnovation, setSocialInnovation] = useState(socialData)
+	const [socialInnovation, setSocialInnovation] = useState(socialData);
+  const [ideation, setIdeation] = useState(Data.Ideation);
+  const [validation, setValidation] = useState(Data.Validation);
 
   //state
 	const [isCheck, setIsCheck] = useState(false);
@@ -25,17 +27,29 @@ const Schedule = ({category, cards}) =>{
 	// Drag functions
   const handleOnDragEnd =(result)=>{
 		if (!result.destination) return;
-		const items = Array.from(socialInnovation);
-		const [reorderItem] = items.splice(result.source.index, 1);
-		items.splice(result.destination.index, 0, reorderItem);
-		setSocialInnovation(items);
+    if (result.source.droppableId === 'social') {
+      const items = Array.from(socialInnovation);
+      const [reorderItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderItem);
+      setSocialInnovation(items);
+    } else if (result.source.droppableId === 'Ideation'){
+      const items = Array.from(ideation);
+      const [reorderItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderItem);
+      setIdeation(items);
+    } else{
+      const items = Array.from(validation);
+      const [reorderItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderItem);
+      setValidation(items);
+    }
 	}
     return (
       <>
 			<DragDropContext onDragEnd={handleOnDragEnd}>
         <div className="main-title">
           <h2>{category === 'socialInnovation'? 'Social Innovation' : ''}</h2>
-          <p className="show-web right">Due Date (Opcional)</p>
+          <p className="show-web right">{category === 'socialInnovation'? 'Due Date (Opcional)' : ''}</p>
         </div>
 				{category === 'socialInnovation'&& socialInnovation.map((card, index) =>(
           <Droppable droppableId="social">
@@ -46,14 +60,14 @@ const Schedule = ({category, cards}) =>{
                     <div className="main-div" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
                       <input className="visible-web" id = {card.id} type="radio" name={card.title} value={card.title} onClick={() => toggle(card.title)} checked={isCheck[card.title]}/>
                       <div className="main-card">
-                        <div className="card">
+                        <div className={ isCheck[card.title]=== true ? "card-border": "card" } >
                           <div >
                             <h3>{card.title}</h3>
-                            <p>view builder</p>
+                            <p className="view-builder">view builder</p>
                           </div>
-                          <img src={Image} alt=""/>
+                          <img src={Image} alt="" className={ isCheck[card.title]=== true ? "img-color": "img-gris" }/>
                         </div>
-                        <div className="show_checked">
+                        <div className={ isCheck[card.title]=== true ? "": "hidden-date" }>
                           <input type="date" name="date" id=""/>
                           <input type="time" name="" id=""/>
                         </div>
@@ -66,47 +80,81 @@ const Schedule = ({category, cards}) =>{
             )}
           </Droppable>
 					))}          
-        </DragDropContext>
+      </DragDropContext>
 			
-				<h2>{category === 'all'? 'Ideation' : ''}</h2>
-				{category === 'all'&& cards.Ideation.map((card) =>(
-          <label key = {card.id}>
-            <input id = {card.id} type="radio" name={card.title} value={card.title} onClick={() => toggle(card.title)} checked={isCheck[card.title]}/>
-            <div>
-							{card.title}
-							
-							<p>view builder</p>
-							
-							<img src={Image} alt=""/>
-						</div>
-						<div className="show_checked">
-							<input type="date" name="date" id=""/>
-							<input type="time" name="" id=""/>
-						</div>
-					</label>
-					))}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <div className="main-title">
+          <h2>{category === 'all'? 'Ideation' : ''}</h2>
+          <button onChange=''>select all in ideation</button>
+          <p className="show-web right">{category === 'all'? 'Due Date (Opcional)' : ''}</p>
+        </div>
+				{category === 'all'&& ideation.map((card, index) =>(
+          <Droppable droppableId={card.type}>
+            {(provided) =>(
+              <label  {...provided.droppableProps} ref={provided.innerRef}>                
+                <Draggable key = {card.id} draggableId={card.id} index={index}>
+                  {(provided) =>(
+                    <div className="main-div" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                      <input className="visible-web" id = {card.id} type="radio" name={card.title} value={card.title} onClick={() => toggle(card.title)} checked={isCheck[card.title]}/>
+                      <div className="main-card">
+                        <div className={ isCheck[card.title]=== true ? "card-border": "card" } >
+                          <div >
+                            <h3>{card.title}</h3>
+                            <p className="view-builder">view builder</p>
+                          </div>
+                          <img src={Image} alt="" className={ isCheck[card.title]=== true ? "img-color": "img-gris" }/>
+                        </div>
+                        <div className={ isCheck[card.title]=== true ? "": "hidden-date" }>
+                          <input type="date" name="date" id=""/>
+                          <input type="time" name="" id=""/>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+                {provided.placeholder}
+              </label>
+            )}
+          </Droppable>
+					))}          
+      </DragDropContext>
 
-				<h2>{category === 'all'? 'Validation' : ''}</h2>
-				<button onChange=''>select all in ideation</button>
-				{category === 'all'&& cards.Validation.map((card) =>(
-					<>
-					
-          <label key = {card.id}>
-            <input id = {card.id} type="radio" name={card.title} value={card.title} onClick={() => toggle(card.title)} checked={isCheck[card.title]}/>
-            <div>
-							{card.title}
-							
-							<p>view builder</p>
-							
-							<img src={Image} alt=""/>
-						</div>
-						<div className="show_checked">
-							<input type="date" name="date" id=""/>
-							<input type="time" name="" id=""/>
-						</div>
-					</label>
-					</>
-					))}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <div className="main-title">
+          <h2>{category === 'all'? 'Validation' : ''}</h2>
+          <button onChange=''>select all in ideation</button>
+          <p className="show-web right">{category === 'all'? 'Due Date (Opcional)' : ''}</p>
+        </div>
+				{category === 'all'&& validation.map((card, index) =>(
+          <Droppable droppableId={card.type}>
+            {(provided) =>(
+              <label  {...provided.droppableProps} ref={provided.innerRef}>                
+                <Draggable key = {card.id} draggableId={card.id} index={index}>
+                  {(provided) =>(
+                    <div className="main-div" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                      <input className="visible-web" id = {card.id} type="radio" name={card.title} value={card.title} onClick={() => toggle(card.title)} checked={isCheck[card.title]}/>
+                      <div className="main-card">
+                        <div className={ isCheck[card.title]=== true ? "card-border": "card" } >
+                          <div >
+                            <h3>{card.title}</h3>
+                            <p className="view-builder">view builder</p>
+                          </div>
+                          <img src={Image} alt="" className={ isCheck[card.title]=== true ? "img-color": "img-gris" }/>
+                        </div>
+                        <div className={ isCheck[card.title]=== true ? "": "hidden-date" }>
+                          <input type="date" name="date" id=""/>
+                          <input type="time" name="" id=""/>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+                {provided.placeholder}
+              </label>
+            )}
+          </Droppable>
+					))}          
+      </DragDropContext>
       </>
     );
     }
