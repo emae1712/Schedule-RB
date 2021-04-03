d11e014be78ff55d382b96b711ca44a76537edf4
emae1712 <melissa.aechegaray@gmail.com>
2021-04-02 19:38:15 -0500

    making a Drag drop and trying to be the all select by category

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,34 +1,36 @@
+
 import React, {useState, useEffect} from "react";
 import Schedule from "./Schedule";
 import Data from "./Data.json";
-
+// console.log(Data);
 const Sidebar = () =>{
   const [category, setCategory] = useState('');
-  const [card, setCard] = useState('');
-  const social = Data.Ideation.concat(Data.Validation)
+  const [cards, setCards] = useState('');
+  // const social = Data.Ideation.concat(Data.Validation)
   // const ideation = Data.Ideation;
   // const validation = Data.Validation;
-  console.log(social);
+  // console.log(social);
 
   useEffect(()=>{
-    setCard(Data)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    setCards(Data)
   }, [category])
+  console.log(cards);
+  
     return (
       <>
       <div className = 'sidebar__container'>
         <button onClick={() => setCategory('all')}>All</button>
         <p className="show-web">Templates</p>
         <button onClick={() => setCategory('socialInnovation')}>Social innovation</button>
-        <select className="show-cellphone" onChange={(event) => setCategory(event.target.value)}>
+        {/* <select className="show-cellphone" onChange={(event) => setCategory(event.target.value)}>
           <option value="socialInnovation">Social innovation</option>
           <option value="Template 1">Template 1</option>
           <option value="Template 2">Template 2</option>
           <option value="Template 3">Template 3</option>
-        </select>
+        </select> */}
       </div>
       <div className = 'schedule__container'>
-        <Schedule cards = {card} category= {category} />
+        <Schedule cards = {cards} category= {category} />
       </div>
       </>
     );
